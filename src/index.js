7df09afd3c79c2b88279b117ba80b85dc8e3cecc
Manuel Arnold <arnold@sipgate.de>
2021-07-29 13:50:01 +0200

    feat: parse appointment and save it in local db
    Co-authored-by: Benjamin Awad <awad@sipgate.de>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { createHistoryModule, sipgateIO } from 'sipgateio';
 import * as dotenv from 'dotenv';
+import { writeDB } from "./db.js";
 dotenv.config();
 
-const TEST_STRING = "Termin: 01.08, 13:00, Haare schneiden";
+const TEST_STRING = "Termin: 30.02, 11:00, Treffen im Park";
 
 async function run() {
     /*
@@ -13,19 +14,53 @@ async function run() {
     const historyEntries = await historyModule.fetchAll();
     console.log(historyEntries.filter(entry => entry.type === 'SMS'));
     */
-   parseSms(TEST_STRING);
+   
+   try {
+       const {subject, date} = parse(TEST_STRING);
+       writeDB(subject, date);
+   } catch (e) {
+       console.warn(e.message);
+   }
+   
 }
 
-function parseSms(smsText) {
-    // remove 'Termin: '
-    const s = smsText.slice(8, smsText.length);
-    // split by comma
+function parse(smsBody) {
+    const s = smsBody.slice(8, smsBody.length);
     let tokens = s.split(",");
     tokens = tokens.map(token => token.trim());
-    const date = parseInt(tokens[0].split(".")[0]);
+    const day = parseInt(tokens[0].split(".")[0]);
     const month = parseInt(tokens[0].split(".")[1]);
-    console.log(date);
-    console.log(month);
+    const hour = parseInt(tokens[1].split(":")[0]);
+    const minute = parseInt(tokens[1].split(":")[1]);
+    const date = new Date(2021, month - 1, day, hour, minute);
+
+    if(!isValidDate(date)) {
+        throw Error('The input for the date could not be parsed, please check your input.');
+    } 
+
+    if(date.getDate() !== day) {
+        throw Error('Sorry, invalid day in month.');
+    } 
+
+    if(date.getMonth() !== month) {
+        throw Error('Sorry, invalid month.');
+    } 
+
+    if(minute !== 0) {
+        throw Error('You can only book an appointment every hour.');
+    }
+
+    if(hour > 15 || hour < 8) {
+        throw Error('You can only book an appointment between 8:00 AM and 3:00 PM.');
+    }
+
+
+    return { subject: tokens[2], date: date };
 }
+
+function isValidDate(date) {
+    return date.getTime() === date.getTime();
+} 
+
 
 run();
