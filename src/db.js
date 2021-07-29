56c97ac380a6855b2341e297359cd03a47ce5c3d
Benjamin Awad <awad@sipgate.de>
2021-07-29 14:15:54 +0200

    chore: format code using prettier
    Co-authored-by: Manuel Arnold <arnold@sipgate.de>

--- src/db.js
+++ src/db.js
@@ -1,47 +1,47 @@
-import { writeFileSync, readFileSync, existsSync} from "fs";
+import { writeFileSync, readFileSync, existsSync } from "fs";
 
 export function readDB() {
-    const buffer = readFileSync("db.json");
-    const json = buffer.toString();
-    return deserialize(json);
+  const buffer = readFileSync("db.json");
+  const json = buffer.toString();
+  return deserialize(json);
 }
 
 export function writeDB(subject, date) {
-    const data = serialize(subject, date);
-    writeFileSync("db.json", data, { flags: "w+"});
+  const data = serialize(subject, date);
+  writeFileSync("db.json", data, { flags: "w+" });
 }
 
 function serialize(subject, date) {
-    const data = {
-            subject
-    };
-    
-    let json = null;
-    if(existsSync("db.json")) {
-        const currentData = readDB();
-        Object.keys(currentData).forEach(_ => {
-            if(!currentData.hasOwnProperty(date)) {
-                currentData[date] = data;
-            } else {
-                throw Error("Sorry, an appointment at this time is already taken");
-            }
-        });
-        json = JSON.stringify(currentData);
-    } else {
-        json = JSON.stringify({[date]: data});
-    }
+  const data = {
+    subject,
+  };
 
-    return json;
-} 
+  let json = null;
+  if (existsSync("db.json")) {
+    const currentData = readDB();
+    Object.keys(currentData).forEach((_) => {
+      if (!currentData.hasOwnProperty(date)) {
+        currentData[date] = data;
+      } else {
+        throw Error("Sorry, an appointment at this time is already taken");
+      }
+    });
+    json = JSON.stringify(currentData);
+  } else {
+    json = JSON.stringify({ [date]: data });
+  }
+
+  return json;
+}
 
 function deserialize(json) {
-    const data = JSON.parse(json, (key, value) => {
-        if(key === 'date') {
-            return new Date(value);
-        }
+  const data = JSON.parse(json, (key, value) => {
+    if (key === "date") {
+      return new Date(value);
+    }
 
-        return value;
-    })
+    return value;
+  });
 
-    return data;
+  return data;
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { createHistoryModule, sipgateIO } from 'sipgateio';
-import * as dotenv from 'dotenv';
+import { createHistoryModule, sipgateIO } from "sipgateio";
+import * as dotenv from "dotenv";
 import { writeDB } from "./db.js";
 dotenv.config();
 
 const TEST_STRING = "Termin: 30.02, 11:00, Treffen im Park";
 
 async function run() {
-    /*
+  /*
     const tokenId = process.env.TOKEN_ID;
     const token = process.env.TOKEN;
     const client = sipgateIO({ tokenId, token });
@@ -14,53 +14,54 @@ async function run() {
     const historyEntries = await historyModule.fetchAll();
     console.log(historyEntries.filter(entry => entry.type === 'SMS'));
     */
-   
-   try {
-       const {subject, date} = parse(TEST_STRING);
-       writeDB(subject, date);
-   } catch (e) {
-       console.warn(e.message);
-   }
-   
+
+  try {
+    const { subject, date } = parse(TEST_STRING);
+    writeDB(subject, date);
+  } catch (e) {
+    console.warn(e.message);
+  }
 }
 
 function parse(smsBody) {
-    const s = smsBody.slice(8, smsBody.length);
-    let tokens = s.split(",");
-    tokens = tokens.map(token => token.trim());
-    const day = parseInt(tokens[0].split(".")[0]);
-    const month = parseInt(tokens[0].split(".")[1]);
-    const hour = parseInt(tokens[1].split(":")[0]);
-    const minute = parseInt(tokens[1].split(":")[1]);
-    const date = new Date(2021, month - 1, day, hour, minute);
+  const s = smsBody.slice(8, smsBody.length);
+  let tokens = s.split(",");
+  tokens = tokens.map((token) => token.trim());
+  const day = parseInt(tokens[0].split(".")[0]);
+  const month = parseInt(tokens[0].split(".")[1]);
+  const hour = parseInt(tokens[1].split(":")[0]);
+  const minute = parseInt(tokens[1].split(":")[1]);
+  const date = new Date(2021, month - 1, day, hour, minute);
 
-    if(!isValidDate(date)) {
-        throw Error('The input for the date could not be parsed, please check your input.');
-    } 
+  if (!isValidDate(date)) {
+    throw Error(
+      "The input for the date could not be parsed, please check your input."
+    );
+  }
 
-    if(date.getDate() !== day) {
-        throw Error('Sorry, invalid day in month.');
-    } 
+  if (date.getDate() !== day) {
+    throw Error("Sorry, invalid day in month.");
+  }
 
-    if(date.getMonth() !== month) {
-        throw Error('Sorry, invalid month.');
-    } 
+  if (date.getMonth() !== month) {
+    throw Error("Sorry, invalid month.");
+  }
 
-    if(minute !== 0) {
-        throw Error('You can only book an appointment every hour.');
-    }
+  if (minute !== 0) {
+    throw Error("You can only book an appointment every hour.");
+  }
 
-    if(hour > 15 || hour < 8) {
-        throw Error('You can only book an appointment between 8:00 AM and 3:00 PM.');
-    }
+  if (hour > 15 || hour < 8) {
+    throw Error(
+      "You can only book an appointment between 8:00 AM and 3:00 PM."
+    );
+  }
 
-
-    return { subject: tokens[2], date: date };
+  return { subject: tokens[2], date: date };
 }
 
 function isValidDate(date) {
-    return date.getTime() === date.getTime();
-} 
-
+  return date.getTime() === date.getTime();
+}
 
 run();
